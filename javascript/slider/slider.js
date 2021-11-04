36d2157ef7827423d5cf1c1d56ce52ee923eb75e
eslam1998-dev <eslam1998-dev>
2021-11-04 11:01:11 +0100

    fix some bus in responsive & end subscribe section

--- javascript/slider/slider.js
+++ javascript/slider/slider.js
@@ -1,24 +1,23 @@
 //products section
-if (window.innerWidth <= 576) {
-    $(".products .flexBox").slick({
-        arrows:false,
-        responsive: [
-            {
-                breakpoint : 1920,
-                settings : "unslick"
+$(".products .flexBox").slick({
+    arrows:false,
+    responsive: [
+        {
+            breakpoint: 9999,
+            settings: "unslick"
+        }
+        ,
+        {
+            breakpoint : 576,
+            settings : {
+                slidesToShow:1,
+                slidesToScroll:1,
+                infinite:true
             }
-            ,
-            {
-                breakpoint : 576,
-                settings : {
-                    slidesToShow:1,
-                    slidesToScroll:1,
-                    infinite:true
-                }
-            }
-        ]
-    })
-}
+        }
+    ]
+})
+
 
 //review section
 $(".review .slider").slick({
@@ -26,4 +25,37 @@ $(".review .slider").slick({
     dots:true,
     autoplay:true,
     speed:1500
+})
+
+//doctors section
+$(".doctor .slider").slick({
+    slidesToScroll:1,
+    slidesToShow:4,
+    infinite:false,
+    prevArrow:`<i class="fas fa-chevron-left"></i>`,
+    nextArrow:`<i class="fas fa-chevron-right"></i>`,
+    centerPadding:"50px",
+    responsive : [
+        {
+            breakpoint : 1200,
+            settings: {
+                slidesToShow:3
+            }
+        }
+        ,
+        {
+            breakpoint : 993,
+            settings: {
+                slidesToShow:2,
+                arrows:false
+            }
+        }
+        , {
+            breakpoint:769,
+            settings : {
+                slidesToShow:1,
+                arrows:false
+            }
+        }
+    ]
 })
